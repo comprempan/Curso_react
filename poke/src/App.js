7c2c64a531002e14234bf64debb621379e6bb1c2
rjjimenez <rjjimenez@paradigmadigital.com>
2018-07-06 14:01:34 +0200

    Creamos nuevo directorio compopnets para guardar los componentes de la app. Creamos el componente PokemonList que será utilizado desde App.js y que pintará la lista de pokemons.

--- poke/src/App.js
+++ poke/src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-
-// mock lista pockemons
-const mockPokemonList = [
-  { name: 'Bulbasaur', url: 'bulbasaur.com' },
-  { name: 'Charmander', url: 'charmander.com' },
-  { name: 'Squirtle', url: 'squirtle.com' },
-];
+import { PokemonList } from './components/PokemonList'
 
 class App extends Component {
   render() {
-    return (
-      <section className="pokemon-list-container">
-        <h2>Pokemon List</h2>
-        <ul className="pokemon-list">
-          {
-            mockPokemonList.map(p => (
-              <li key={p.name}>
-                <button className="pokemon-list-item">
-                  {p.name}
-                </button>
-              </li>
-            ))
-          }
-        </ul>
-      </section>
-    );
+    return <PokemonList />
   }
 }
 
