24a78ddd97d377dc498173feb2d122f93c18869e
rjjimenez <rjjimenez@paradigmadigital.com>
2018-07-06 14:13:21 +0200

    Usamos la lista de pokemons como variable de la App y se pasará al componente PokemonList.js como propiedad.

--- poke/src/App.js
+++ poke/src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import { PokemonList } from './components/PokemonList'
 
+// mock lista pockemons
+const mockPokemonList = [
+  { name: 'Bulbasaur', url: 'bulbasaur.com' },
+  { name: 'Charmander', url: 'charmander.com' },
+  { name: 'Squirtle', url: 'squirtle.com' },
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <PokemonList />
+    return <PokemonList pokemonList={mockPokemonList}/>
   }
 }
 
--- poke/src/components/PokemonList.js
+++ poke/src/components/PokemonList.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
-// mock lista pockemons
-const mockPokemonList = [
-  { name: 'Bulbasaur', url: 'bulbasaur.com' },
-  { name: 'Charmander', url: 'charmander.com' },
-  { name: 'Squirtle', url: 'squirtle.com' },
-];
-
 export const PokemonList = (props) => {
   return (
       <section className="pokemon-list-container">
           <h2>Pokemon List</h2>
           <ul className="pokemon-list">
           {
-              mockPokemonList.map(p => (
-              <li key={p.name}>
+            props.pokemonList.map(pokemon => (
+              <li key={pokemon.name}>
                   <button className="pokemon-list-item">
-                  {p.name}
+                  {pokemon.name};
                   </button>
               </li>
               ))
